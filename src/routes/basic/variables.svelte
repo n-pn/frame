7186bcdaf688402edc1150a182c2b8f4d8c2ed35
Nipinium <nipinium@gmail.com>
2019-09-15 22:21:38 +0700

    add rulers

--- src/routes/basic/variables.svelte
+++ src/routes/basic/variables.svelte
@@ -10,27 +10,35 @@
     "purple",
     "pink"
   ];
-  const tones = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const tones = [9, 8, 7, 6, 5, 4, 3, 2, 1];
 </script>
 
 <style lang="scss">
   @import "helpers";
+
+  color-table {
+    display: block;
+    background-color: #fff;
+    padding: 1rem;
+    @include corner(md);
+    @include shadow(md);
+  }
+
   color-list {
     display: block;
-    padding: 0.5rem 0;
+    // padding: 0.5rem 0;
     @include clearfix;
   }
 
   color-name {
     font-variant: small-caps;
-    width: 4rem;
     line-height: 2rem;
     float: left;
     @include color(neutral, 6);
   }
 
   color-item {
-    float: left;
+    float: right;
     width: 2rem;
     height: 2rem;
     // @include shadow(md);
@@ -45,19 +53,25 @@
       }
     }
   }
+  hr {
+    margin: 0.5rem 0;
+  }
 </style>
 
 <div class="m-pretty">
   <h1>Colors</h1>
 
-  <h2>Palette</h2>
+  <color-table>
+    <h2>Palette</h2>
+    {#each colors as color}
+      <hr />
+      <color-list>
+        <color-name>{color}:</color-name>
+        {#each tones as tone}
+          <color-item {color} {tone} />
+        {/each}
+      </color-list>
+    {/each}
+  </color-table>
 
-  {#each colors as color}
-    <color-list>
-      <color-name>{color}:</color-name>
-      {#each tones as tone}
-        <color-item {color} {tone} />
-      {/each}
-    </color-list>
-  {/each}
 </div>
