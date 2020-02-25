79a98566dbeca4f16ab60ea7a080ce21244e4fdd
Nipinium <nipinium@gmail.com>
2020-02-25 22:39:48 +0700

    add simple text inputs

---
+++ web/src/routes/molds/inputs.svelte
@@ -0,0 +1,44 @@
+<style type="text/scss">
+
+</style>
+
+<article m-article>
+  <h1>Inputs</h1>
+
+  <h2>Text fields</h2>
+
+  <div class="u-stack">
+
+    <div class="u-flow">
+      <div m-input="small">
+        <input type="text" name="#t1" />
+        <label for="t1">Text 1</label>
+      </div>
+
+      <button m-button="small">Submit</button>
+    </div>
+
+    <div class="u-flow">
+      <div m-input>
+        <input type="text" name="#t2" />
+        <label for="t2">Text 2</label>
+      </div>
+
+      <button m-button="primary">Submit</button>
+    </div>
+
+    <div class="u-flow">
+      <div m-input="large">
+        <input type="text" name="#t3" />
+        <label for="t3">Text 3</label>
+      </div>
+
+      <button m-button="large line harmful">Submit</button>
+    </div>
+
+    <div m-input>
+      <textarea name="" id="" cols="30" rows="10" />
+    </div>
+  </div>
+
+</article>
